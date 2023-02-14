2b869ac8bc1a911a16a56383c69c4a05d4b8e429
nakuljain20 <109940238+nakuljain20@users.noreply.github.com>
2023-02-14 16:37:16 +0530

    Update dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -379,11 +379,11 @@ userGoneGames = len(df[df["userGone"] == True])
 stuckGames = len(df[df["stuckGame"] == True])
 notEndGames = len(df[df["gameEnd"] == False])
 
-columns = ["User Gone Games", "Stuck Games", "Not Ended Games"]
+columnName = ["User Gone Games", "Stuck Games", "Not Ended Games"]
 value = [userGoneGames, stuckGames, notEndGames]
 
-# chart_data = pd.DataFrame(index=columns, columns=value)
-st.bar_chart(value)
+chart_data = pd.DataFrame({'index' : columnName, 'number of games' : value}).set_index('index')
+st.bar_chart(chart_data)
 
 turnCount = st.text_input("Enter Turn Count: ", value="1")
 
